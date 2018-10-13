15d5b974774331d8815fb83eefba8c9dc05c2d82
Arthur Vanhoorebeke <avanhoorebeke@gmail.com>
2018-10-13 14:04:08 +0200

    added comments in code

--- test1.py
+++ test1.py
@@ -12,16 +12,19 @@ def checkBit(bit,number):
   else:
     return False
 
-
+#initialise ft232
 ft232h = GpioController()
-
+#open from url
 ft232h.configure("ftdi://ftdi:ft232h/1")
 
 print("connected? " + str(ft232h.is_connected))
 print(ft232h.pins)
 
+#sets direction for all pins - Only the fourth pin (D3) is OUTPUT
+#0x08 = 8 = 1000
 ft232h.set_direction(0xFF,0x08)
 
+#If the 8th pin reads TRUE, fourth pin gets written to HIGH
 while True:
   if checkBit(8,ft232h.read() ):
     print("input in D7 is TRUE => light ON")
